258ffa4aab39644d4dbcf932b1d186b3108aec6d
Alemarius Nexus <alemariusnexus@gmail.com>
2020-07-19 00:22:16 +0200

    PatternSearch additions: Support for address-aligned search; Use handler functions (e.g. lambda) on match; stop search prematurely if requested by handler function (e.g. when searching for a single match only).

--- src/BlackBone/Patterns/PatternSearch.cpp
+++ src/BlackBone/Patterns/PatternSearch.cpp
@@ -9,28 +9,28 @@
 namespace blackbone
 {
 
-PatternSearch::PatternSearch( const std::vector<uint8_t>& pattern )
-    : _pattern( pattern )
+PatternSearch::PatternSearch( const std::vector<uint8_t>& pattern, size_t logAlignment /*= 0*/ )
+    : _pattern( pattern ), logAlignment(logAlignment)
 {
 }
 
-PatternSearch::PatternSearch( const std::initializer_list<uint8_t>&& pattern )
-    : _pattern( pattern )
+PatternSearch::PatternSearch( const std::initializer_list<uint8_t>&& pattern, size_t logAlignment /*= 0*/ )
+    : _pattern( pattern ), logAlignment(logAlignment)
 {
 }
 
-PatternSearch::PatternSearch( const std::string& pattern )
-    : _pattern( pattern.begin(), pattern.end() )
+PatternSearch::PatternSearch( const std::string& pattern, size_t logAlignment /*= 0*/ )
+    : _pattern( pattern.begin(), pattern.end() ), logAlignment(logAlignment)
 {
 }
 
-PatternSearch::PatternSearch( const char* pattern, size_t len /*= 0*/ )
-    : _pattern( pattern, pattern + (len ? len : strlen( pattern )) )
+PatternSearch::PatternSearch( const char* pattern, size_t len /*= 0*/, size_t logAlignment /*= 0*/ )
+    : _pattern( pattern, pattern + (len ? len : strlen( pattern )) ), logAlignment(logAlignment)
 {
 }
 
-PatternSearch::PatternSearch( const uint8_t* pattern, size_t len /*= 0*/ )
-    : _pattern( pattern, pattern + (len ? len : strlen( (const char*)pattern )) )
+PatternSearch::PatternSearch( const uint8_t* pattern, size_t len /*= 0*/, size_t logAlignment /*= 0*/ )
+    : _pattern( pattern, pattern + (len ? len : strlen( (const char*)pattern )) ), logAlignment(logAlignment)
 { 
 }
 
@@ -44,37 +44,42 @@ PatternSearch::PatternSearch( const uint8_t* pattern, size_t len /*= 0*/ )
 /// <param name="out">Found results</param>
 /// <param name="value_offset">Value that will be added to resulting addresses</param>
 /// <returns>Number of found addresses</returns>
-size_t PatternSearch::Search( 
-    uint8_t wildcard, 
-    void* scanStart, 
-    size_t scanSize, 
-    std::vector<ptr_t>& out, 
-    ptr_t value_offset /*= 0*/ 
+bool PatternSearch::SearchWithHandler(
+    uint8_t wildcard,
+        void* scanStart,
+        size_t scanSize,
+		MatchHandler handler,
+        ptr_t value_offset /*= 0*/
     ) const
 {
     const uint8_t* cstart = (const uint8_t*)scanStart;
     const uint8_t* cend   = cstart + scanSize;
+
+    // TODO: Would it be beneficial to use logAlignment here as well?
 
     auto comparer = [&wildcard]( uint8_t val1, uint8_t val2 )
     {
         return (val1 == val2 || val2 == wildcard);
     };
 
-    for (;;)
+    bool running = true;
+    while (running)
     {
         const uint8_t* res = std::search( cstart, cend, _pattern.begin(), _pattern.end(), comparer );
         if (res >= cend)
             break;
 
         if (value_offset != 0)
-            out.emplace_back( REBASE( res, scanStart, value_offset ) );
+        	running = !handler( REBASE( res, scanStart, value_offset ) );
+            //out.emplace_back( REBASE( res, scanStart, value_offset ) );
         else
-            out.emplace_back( reinterpret_cast<ptr_t>(res) );
+            //out.emplace_back( reinterpret_cast<ptr_t>(res) );
+        	running = !handler( reinterpret_cast<ptr_t>(res) );
 
         cstart = res + _pattern.size();
     }
 
-    return out.size();
+    return !running;
 }
 
 /// <summary>
@@ -86,20 +91,23 @@ size_t PatternSearch::Search(
 /// <param name="out">Found results</param>
 /// <param name="value_offset">Value that will be added to resulting addresses</param>
 /// <returns>Number of found addresses</returns>
-size_t PatternSearch::Search( 
-    void* scanStart,
-    size_t scanSize, 
-    std::vector<ptr_t>& out,
-    ptr_t value_offset /*= 0*/ 
-    ) const
+bool PatternSearch::SearchWithHandler(
+	void* scanStart,
+	size_t scanSize,
+	MatchHandler handler,
+	ptr_t value_offset /*= 0*/
+	) const
 {
-    size_t bad_char_skip[UCHAR_MAX + 1];
+	size_t bad_char_skip[UCHAR_MAX + 1];
 
     const uint8_t* haystack = reinterpret_cast<const uint8_t*>(scanStart);
+    const uint8_t* haystackEnd = haystack + scanSize - _pattern.size();
     const uint8_t* needle   = &_pattern[0];
     uintptr_t       nlen     = _pattern.size();
     uintptr_t       scan     = 0;
     uintptr_t       last     = nlen - 1;
+    size_t alignMask		 = 0xFFFFFFFFFFFFFFFFL << logAlignment;
+    size_t alignOffs		 = (1 << logAlignment) - 1;
 
     //
     // Preprocess
@@ -113,26 +121,33 @@ size_t PatternSearch::Search(
     //
     // Search
     //
-    while (scanSize >= static_cast<size_t>(nlen))
+    bool running = true;
+    //while (haystack <= haystackEnd  &&  out.size() < maxMatches)
+    while (haystack <= haystackEnd  &&  running)
     {
         for (scan = last; haystack[scan] == needle[scan]; --scan)
         {
             if (scan == 0)
             {
                 if (value_offset != 0)
-                    out.emplace_back( REBASE( haystack, scanStart, value_offset ) );
+                    //out.emplace_back( REBASE( haystack, scanStart, value_offset ) );
+                	running = !handler( REBASE( haystack, scanStart, value_offset ) );
                 else
-                    out.emplace_back( reinterpret_cast<ptr_t>(haystack) );
+                    //out.emplace_back( reinterpret_cast<ptr_t>(haystack) );
+                	running = !handler( reinterpret_cast<ptr_t>(haystack) );
 
                 break;
             }
         }
 
-        scanSize -= bad_char_skip[haystack[last]];
         haystack += bad_char_skip[haystack[last]];
+
+        if (logAlignment != 0) {
+        	haystack = (const uint8_t*) (size_t(haystack+alignOffs) & alignMask);
+        }
     }
 
-    return out.size();
+    return !running;
 }
 
 /// <summary>
@@ -144,23 +159,24 @@ size_t PatternSearch::Search(
 /// <param name="scanSize">Size of region to scan</param>
 /// <param name="out">Found results</param>
 /// <returns>Number of found addresses</returns>
-size_t PatternSearch::SearchRemote( 
-    Process& remote, 
-    uint8_t wildcard, 
-    ptr_t scanStart, 
-    size_t scanSize, 
-    std::vector<ptr_t>& out 
+bool PatternSearch::SearchRemoteWithHandler(
+    Process& remote,
+    uint8_t wildcard,
+    ptr_t scanStart,
+    size_t scanSize,
+    MatchHandler handler
     ) const
 {
     uint8_t *pBuffer = reinterpret_cast<uint8_t*>(VirtualAlloc( NULL, scanSize, MEM_COMMIT, PAGE_READWRITE ));
 
+    bool stopped = false;
     if (pBuffer && remote.memory().Read( scanStart, scanSize, pBuffer ) == STATUS_SUCCESS)
-        Search( wildcard, pBuffer, scanSize, out, scanStart );
+    	stopped = SearchWithHandler( wildcard, pBuffer, scanSize, handler, scanStart );
 
     if (pBuffer)
         VirtualFree( pBuffer, 0, MEM_RELEASE );
 
-    return out.size();
+    return stopped;
 }
 
 /// <summary>
@@ -171,22 +187,23 @@ size_t PatternSearch::SearchRemote(
 /// <param name="scanSize">Size of region to scan</param>
 /// <param name="out">Found results</param>
 /// <returns>Number of found addresses</returns>
-size_t PatternSearch::SearchRemote( 
-    Process& remote, 
-    ptr_t scanStart, 
-    size_t scanSize, 
-    std::vector<ptr_t>& out 
+bool PatternSearch::SearchRemoteWithHandler(
+    Process& remote,
+    ptr_t scanStart,
+    size_t scanSize,
+    MatchHandler handler
     ) const
 {
     uint8_t *pBuffer = reinterpret_cast<uint8_t*>(VirtualAlloc( NULL, scanSize, MEM_COMMIT, PAGE_READWRITE ));
 
+    bool stopped = false;
     if (pBuffer && remote.memory().Read( scanStart, scanSize, pBuffer ) == STATUS_SUCCESS)
-        Search( pBuffer, scanSize, out, scanStart );
+    	stopped = SearchWithHandler( pBuffer, scanSize, handler, scanStart );
 
     if (pBuffer)
         VirtualFree( pBuffer, 0, MEM_RELEASE );
 
-    return out.size();
+    return stopped;
 }
 
 /// <summary>
@@ -197,22 +214,21 @@ size_t PatternSearch::SearchRemote(
 /// <param name="wildcard">Pattern wildcard</param>
 /// <param name="out">Found results</param>
 /// <returns>Number of found addresses</returns>
-size_t PatternSearch::SearchRemoteWhole( 
-    Process& remote, 
-    bool useWildcard, 
-    uint8_t wildcard, 
-    std::vector<ptr_t>& out 
+bool PatternSearch::SearchRemoteWholeWithHandler(
+    Process& remote,
+    bool useWildcard,
+    uint8_t wildcard,
+    MatchHandler handler
     ) const
 {
     MEMORY_BASIC_INFORMATION64 mbi = { 0 };
     size_t  bufsize = 1 * 1024 * 1024;  // 1 MB
     uint8_t *buf = reinterpret_cast<uint8_t*>(VirtualAlloc( 0, bufsize, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE ));
 
-    out.clear();
-
     auto native = remote.core().native();
 
-    for (ptr_t memptr = native->minAddr(); memptr < native->maxAddr(); memptr = mbi.BaseAddress + mbi.RegionSize)
+    bool running = true;
+    for (ptr_t memptr = native->minAddr(); memptr < native->maxAddr()  &&  running; memptr = mbi.BaseAddress + mbi.RegionSize)
     {
         auto status = remote.core().native()->VirtualQueryExT( memptr, &mbi );
 
@@ -237,14 +253,150 @@ size_t PatternSearch::SearchRemoteWhole(
             continue;
 
         if (useWildcard)
-            Search( wildcard, buf, static_cast<size_t>(mbi.RegionSize), out, memptr );
+        	running = !SearchWithHandler( wildcard, buf, static_cast<size_t>(mbi.RegionSize), handler, memptr );
         else
-            Search( buf, static_cast<size_t>(mbi.RegionSize), out, memptr );
+        	running = !SearchWithHandler( buf, static_cast<size_t>(mbi.RegionSize), handler, memptr );
     }
 
     VirtualFree( buf, 0, MEM_RELEASE );
 
-    return out.size();
+    return !running;
+}
+
+
+
+
+/// <summary>
+/// Default pattern matching with wildcards.
+/// std::search is approximately 2x faster than naive approach.
+/// </summary>
+/// <param name="wildcard">Pattern wildcard</param>
+/// <param name="scanStart">Starting address</param>
+/// <param name="scanSize">Size of region to scan</param>
+/// <param name="out">Found results</param>
+/// <param name="value_offset">Value that will be added to resulting addresses</param>
+/// <returns>Number of found addresses</returns>
+size_t PatternSearch::Search(
+    uint8_t wildcard,
+    void* scanStart,
+    size_t scanSize,
+    std::vector<ptr_t>& out,
+    ptr_t value_offset /*= 0*/,
+	size_t maxMatches /*= SIZE_MAX*/
+    ) const
+{
+	if (out.size() >= maxMatches)
+		return out.size();
+
+	auto handler = std::bind(PatternSearch::collectAllMatchHandler, std::placeholders::_1, std::ref(out), maxMatches);
+	SearchWithHandler(wildcard, scanStart, scanSize, handler, value_offset);
+
+	return out.size();
+}
+
+/// <summary>
+/// Full pattern match, no wildcards.
+/// Uses Boyer–Moore–Horspool algorithm.
+/// </summary>
+/// <param name="scanStart">Starting address</param>
+/// <param name="scanSize">Size of region to scan</param>
+/// <param name="out">Found results</param>
+/// <param name="value_offset">Value that will be added to resulting addresses</param>
+/// <returns>Number of found addresses</returns>
+size_t PatternSearch::Search( 
+    void* scanStart,
+    size_t scanSize, 
+    std::vector<ptr_t>& out,
+    ptr_t value_offset /*= 0*/,
+	size_t maxMatches /*= SIZE_MAX*/
+    ) const
+{
+	if (out.size() >= maxMatches)
+		return out.size();
+
+	auto handler = std::bind(PatternSearch::collectAllMatchHandler, std::placeholders::_1, std::ref(out), maxMatches);
+	SearchWithHandler(scanStart, scanSize, handler, value_offset);
+
+	return out.size();
+}
+
+/// <summary>
+/// Search pattern in remote process
+/// </summary>
+/// <param name="remote">Remote process</param>
+/// <param name="wildcard">Pattern wildcard</param>
+/// <param name="scanStart">Starting address</param>
+/// <param name="scanSize">Size of region to scan</param>
+/// <param name="out">Found results</param>
+/// <returns>Number of found addresses</returns>
+size_t PatternSearch::SearchRemote( 
+    Process& remote, 
+    uint8_t wildcard, 
+    ptr_t scanStart, 
+    size_t scanSize, 
+    std::vector<ptr_t>& out,
+	size_t maxMatches /*= SIZE_MAX*/
+    ) const
+{
+	if (out.size() >= maxMatches)
+		return out.size();
+
+	auto handler = std::bind(PatternSearch::collectAllMatchHandler, std::placeholders::_1, std::ref(out), maxMatches);
+	SearchRemoteWithHandler(remote, wildcard, scanStart, scanSize, handler);
+
+	return out.size();
+}
+
+/// <summary>
+/// Search pattern in remote process
+/// </summary>
+/// <param name="remote">Remote process</param>
+/// <param name="scanStart">Starting address</param>
+/// <param name="scanSize">Size of region to scan</param>
+/// <param name="out">Found results</param>
+/// <returns>Number of found addresses</returns>
+size_t PatternSearch::SearchRemote( 
+    Process& remote, 
+    ptr_t scanStart, 
+    size_t scanSize, 
+    std::vector<ptr_t>& out,
+	size_t maxMatches /*= SIZE_MAX*/
+    ) const
+{
+	if (out.size() >= maxMatches)
+		return out.size();
+
+	auto handler = std::bind(PatternSearch::collectAllMatchHandler, std::placeholders::_1, std::ref(out), maxMatches);
+	SearchRemoteWithHandler(remote, scanStart, scanSize, handler);
+
+	return out.size();
+}
+
+/// <summary>
+/// Search pattern in whole address space of remote process
+/// </summary>
+/// <param name="remote">Remote process</param>
+/// <param name="useWildcard">True if pattern contains wildcards</param>
+/// <param name="wildcard">Pattern wildcard</param>
+/// <param name="out">Found results</param>
+/// <returns>Number of found addresses</returns>
+size_t PatternSearch::SearchRemoteWhole( 
+    Process& remote, 
+    bool useWildcard, 
+    uint8_t wildcard, 
+    std::vector<ptr_t>& out,
+	size_t maxMatches /*= SIZE_MAX*/
+    ) const
+{
+	out.clear();
+
+	if (out.size() >= maxMatches)
+		return out.size();
+
+	auto handler = std::bind(PatternSearch::collectAllMatchHandler, std::placeholders::_1, std::ref(out), maxMatches);
+	SearchRemoteWholeWithHandler(remote, useWildcard, wildcard, handler);
+
+	return out.size();
 }
 
 
--- src/BlackBone/Patterns/PatternSearch.h
+++ src/BlackBone/Patterns/PatternSearch.h
@@ -4,6 +4,7 @@
 
 #include <string>
 #include <vector>
+#include <functional>
 #include <initializer_list>
 
 namespace blackbone
@@ -12,13 +13,110 @@ namespace blackbone
 class PatternSearch
 {
 public:
-    BLACKBONE_API PatternSearch( const std::vector<uint8_t>& pattern );
-    BLACKBONE_API PatternSearch( const std::initializer_list<uint8_t>&& pattern );
-    BLACKBONE_API PatternSearch( const std::string& pattern );
-    BLACKBONE_API PatternSearch( const char* pattern, size_t len = 0 );
-    BLACKBONE_API PatternSearch( const uint8_t* pattern, size_t len = 0 );
+	/// <summary>
+	/// Callback to handle a matching address for the Search*WithHandler() methods.
+	/// If the handler returns true, the search is stopped, else the search continues.
+	/// </summary>
+	typedef std::function<bool (ptr_t)> MatchHandler;
+
+public:
+	// logAlignment can be used to speed-up the search in some cases. For example, if you know that the start of the pattern
+	// is always 8-byte-aligned, you can pass logAlignment=3 (2^3 = 8) to skip searching at all addresses that aren't multiples
+	// of 8. Note that for smaller alignments and depending on the exact pattern, this may not always be faster (it may even be
+	// a tiny bit slower), so profile it if you care about performance.
+    BLACKBONE_API PatternSearch( const std::vector<uint8_t>& pattern, size_t logAlignment = 0 );
+    BLACKBONE_API PatternSearch( const std::initializer_list<uint8_t>&& pattern, size_t logAlignment = 0 );
+    BLACKBONE_API PatternSearch( const std::string& pattern, size_t logAlignment = 0 );
+    BLACKBONE_API PatternSearch( const char* pattern, size_t len = 0, size_t logAlignment = 0 );
+    BLACKBONE_API PatternSearch( const uint8_t* pattern, size_t len = 0, size_t logAlignment = 0 );
 
     BLACKBONE_API ~PatternSearch() = default;
+
+    /// <summary>
+    /// Default pattern matching with wildcards and a callback handler for matches.
+    /// std::search is approximately 2x faster than naive approach.
+    /// </summary>
+    /// <param name="wildcard">Pattern wildcard</param>
+    /// <param name="scanStart">Starting address</param>
+    /// <param name="scanSize">Size of region to scan</param>
+    /// <param name="handler">Callback that is called for every match. If it returns true, the search is stopped prematurely.</param>
+    /// <param name="value_offset">Value that will be added to resulting addresses</param>
+    /// <returns>true if the callback handler ever returned true (i.e. the search ended prematurely), false otherwise.</returns>
+    BLACKBONE_API bool SearchWithHandler(
+        uint8_t wildcard,
+        void* scanStart,
+        size_t scanSize,
+		MatchHandler handler,
+        ptr_t value_offset = 0
+        ) const;
+
+    /// <summary>
+    /// Full pattern match, no wildcards, with a callback handler for matches.
+    /// Uses Boyer–Moore–Horspool algorithm.
+    /// </summary>
+    /// <param name="scanStart">Starting address</param>
+    /// <param name="scanSize">Size of region to scan</param>
+    /// <param name="handler">Callback that is called for every match. If it returns true, the search is stopped prematurely.</param>
+    /// <param name="value_offset">Value that will be added to resulting addresses</param>
+    /// <returns>true if the callback handler ever returned true (i.e. the search ended prematurely), false otherwise.</returns>
+    BLACKBONE_API bool SearchWithHandler(
+        void* scanStart,
+        size_t scanSize,
+		MatchHandler handler,
+        ptr_t value_offset = 0
+        ) const;
+
+    /// <summary>
+    /// Search pattern in remote process with a callback handler for matches
+    /// </summary>
+    /// <param name="remote">Remote process</param>
+    /// <param name="wildcard">Pattern wildcard</param>
+    /// <param name="scanStart">Starting address</param>
+    /// <param name="scanSize">Size of region to scan</param>
+    /// <param name="handler">Callback that is called for every match. If it returns true, the search is stopped prematurely.</param>
+    /// <returns>true if the callback handler ever returned true (i.e. the search ended prematurely), false otherwise.</returns>
+    BLACKBONE_API bool SearchRemoteWithHandler(
+        class Process& remote,
+        uint8_t wildcard,
+        ptr_t scanStart,
+        size_t scanSize,
+        MatchHandler handler
+        ) const;
+
+    /// <summary>
+    /// Search pattern in remote process with a callback handler for matches
+    /// </summary>
+    /// <param name="remote">Remote process</param>
+    /// <param name="scanStart">Starting address</param>
+    /// <param name="scanSize">Size of region to scan</param>
+    /// <param name="handler">Callback that is called for every match. If it returns true, the search is stopped prematurely.</param>
+    /// <returns>true if the callback handler ever returned true (i.e. the search ended prematurely), false otherwise.</returns>
+    BLACKBONE_API bool SearchRemoteWithHandler(
+        class Process& remote,
+        ptr_t scanStart,
+        size_t scanSize,
+        MatchHandler handler
+        ) const;
+
+    /// <summary>
+    /// Search pattern in whole address space of remote process with a callback handler for matches
+    /// </summary>
+    /// <param name="remote">Remote process</param>
+    /// <param name="useWildcard">True if pattern contains wildcards</param>
+    /// <param name="wildcard">Pattern wildcard</param>
+    /// <param name="handler">Callback that is called for every match. If it returns true, the search is stopped prematurely.</param>
+    /// <returns>true if the callback handler ever returned true (i.e. the search ended prematurely), false otherwise.</returns>
+    BLACKBONE_API bool SearchRemoteWholeWithHandler(
+        class Process& remote,
+        bool useWildcard,
+        uint8_t wildcard,
+        MatchHandler handler
+        ) const;
+
+
+
+
+
 
     /// <summary>
     /// Default pattern matching with wildcards.
@@ -29,13 +127,15 @@ public:
     /// <param name="scanSize">Size of region to scan</param>
     /// <param name="out">Found results</param>
     /// <param name="value_offset">Value that will be added to resulting addresses</param>
+    /// <param name="maxMatches">Maximum number of matches to collect</param>
     /// <returns>Number of found addresses</returns>
     BLACKBONE_API size_t Search( 
         uint8_t wildcard, 
         void* scanStart, 
         size_t scanSize, 
         std::vector<ptr_t>& out, 
-        ptr_t value_offset = 0 
+        ptr_t value_offset = 0,
+		size_t maxMatches = SIZE_MAX
         ) const;
 
     /// <summary>
@@ -46,12 +146,14 @@ public:
     /// <param name="scanSize">Size of region to scan</param>
     /// <param name="out">Found results</param>
     /// <param name="value_offset">Value that will be added to resulting addresses</param>
+    /// <param name="maxMatches">Maximum number of matches to collect</param>
     /// <returns>Number of found addresses</returns>
     BLACKBONE_API size_t Search( 
         void* scanStart, 
         size_t scanSize, 
         std::vector<ptr_t>& out, 
-        ptr_t value_offset = 0 
+        ptr_t value_offset = 0,
+		size_t maxMatches = SIZE_MAX
         ) const;
 
     /// <summary>
@@ -62,13 +164,15 @@ public:
     /// <param name="scanStart">Starting address</param>
     /// <param name="scanSize">Size of region to scan</param>
     /// <param name="out">Found results</param>
+    /// <param name="maxMatches">Maximum number of matches to collect</param>
     /// <returns>Number of found addresses</returns>
     BLACKBONE_API size_t SearchRemote( 
         class Process& remote, 
         uint8_t wildcard, 
         ptr_t scanStart,
         size_t scanSize, 
-        std::vector<ptr_t>& out 
+        std::vector<ptr_t>& out,
+		size_t maxMatches = SIZE_MAX
         ) const;
 
     /// <summary>
@@ -78,12 +182,14 @@ public:
     /// <param name="scanStart">Starting address</param>
     /// <param name="scanSize">Size of region to scan</param>
     /// <param name="out">Found results</param>
+    /// <param name="maxMatches">Maximum number of matches to collect</param>
     /// <returns>Number of found addresses</returns>
     BLACKBONE_API size_t SearchRemote( 
         class Process& remote, 
         ptr_t scanStart, 
         size_t scanSize, 
-        std::vector<ptr_t>& out 
+        std::vector<ptr_t>& out,
+		size_t maxMatches = SIZE_MAX
         ) const;
 
     /// <summary>
@@ -93,16 +199,26 @@ public:
     /// <param name="useWildcard">True if pattern contains wildcards</param>
     /// <param name="wildcard">Pattern wildcard</param>
     /// <param name="out">Found results</param>
+    /// <param name="maxMatches">Maximum number of matches to collect</param>
     /// <returns>Number of found addresses</returns>
     BLACKBONE_API size_t SearchRemoteWhole( 
         class Process& remote, 
         bool useWildcard, 
         uint8_t wildcard, 
-        std::vector<ptr_t>& out 
+        std::vector<ptr_t>& out,
+		size_t maxMatches = SIZE_MAX
         ) const;
 
 private:
+    static inline bool collectAllMatchHandler(ptr_t addr, std::vector<ptr_t>& out, size_t maxMatches)
+    {
+    	out.emplace_back(addr);
+		return out.size() >= maxMatches;
+    }
+
+private:
     std::vector<uint8_t> _pattern;      // Pattern to search
+    size_t logAlignment;
 };
 
 }
